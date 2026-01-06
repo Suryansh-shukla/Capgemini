83e0833fa1b507890bce740d53bcecaa7eb185e9
Suryansh Shukla <ssuryansh678@gmail.com>
2026-01-06 11:48:27 +0530

    Commit of Customers data

---
+++ Day11/CustomePropertyDemo/CustomePropertyDemo/Orders.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomePropertyDemo
+{
+    class Orders
+    {
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
