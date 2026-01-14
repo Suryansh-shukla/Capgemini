b546d489186f3eb6e67b8b581eab629f7703b0ff
Suryansh Shukla <ssuryansh678@gmail.com>
2026-01-14 13:39:47 +0530

    Commits from Suryansh on 14/01/2026

---
+++ 14-01-2026/AssemblyDemoProj/UILogic/Doctor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UILogic
+{
+    [AttributeUsage(AttributeTargets.Class|AttributeTargets.Property|AttributeTargets.Method)]
+    class DoctorAttribute: Attribute
+    {
+        public string Name { get; set; }
+        public string CheckedOnDate { get; set; }
+    }
+}
--- 14-01-2026/AssemblyDemoProj/UILogic/Program.cs
+++ 14-01-2026/AssemblyDemoProj/UILogic/Program.cs
@@ -7,6 +7,9 @@ using ServiceLibrary;
 
 namespace UILogic
 {
+    [DoctorAttribute(Name ="Dr. Neha", CheckedOnDate ="2024-06-01")]
+    //[DoctorAttribute(Name = "Dr. Amit", CheckedOnDate = "2024-06-02")]
+    //[DoctorAttribute(Name = "Dr. Suresh", CheckedOnDate = "2024-06-03")]
     class Program
     {
         static void Main(string[] args)
@@ -26,6 +29,15 @@ namespace UILogic
             int numResult=logic.AddMe(num1, num2);
             Console.WriteLine($"The Sum of {num1} and {num2} is : {numResult}");
             Console.ReadLine();
+            DoctorAttribute[] attrs = (DoctorAttribute[])Attribute.GetCustomAttributes(typeof(Program), typeof(DoctorAttribute));
+            if(attrs=null)
+            {
+                foreach(var attr in attrs)
+                {
+                    Console.WriteLine($"Checked By: {attr.Name} on Date: {attr.CheckedOnDate}");
+                }
+
+            }
         }
     }
 }
