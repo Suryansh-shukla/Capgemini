961ff41b042e2e3c25690b0d889656306ee1ca74
Suryansh Shukla <ssuryansh678@gmail.com>
2026-02-03 15:13:58 +0530

    NUNit Question of TopBrains

---
+++ Questions/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public decimal Balance { get; set; }
+
+        public Program(decimal balance)
+        {
+            this.Balance = balance; 
+        }
+        public decimal Deposit(decimal amount)
+        {
+            // Validate input and update balance; do not use an empty try block.
+            if (amount <= 0)
+            {
+                throw new ArgumentException("Deposit amount cannot be negative");
+            }
+
+            this.Balance += amount;
+            return this.Balance;
+        }
+        public void Withdraw(decimal amount)
+        {
+            if (amount > this.Balance)
+            {
+                throw new ArgumentException("Insufficient funds)");
+            }
+            this.Balance -= amount;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Questions/ConsoleApp1/ConsoleApp1/UnitTest.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class UnitTest
+    {
+        public void Test_Deposit_ValidAmount() 
+        {
+            Program PObj=new Program(1000m);
+            decimal newBalance=PObj.Deposit(500m);
+            Assert.AreEqual(1500, newBalance);
+        }
+        public void Test_Deposit_NegativeAmount() 
+        {
+            Assert.AreEqual(
+                "Deposit amount cannot be negative",
+                Assert.Throws<Exception>(() => account.Deposit(-200m))!.Message
+            );
+        }
+        public void Test_Withdraw_ValidAmount() 
+        {
+            Program account = new Program(1000m);
+
+            // Act
+            account.Withdraw(400m);
+
+            // Assert (ONLY ONE ASSERT)
+            Assert.AreEqual(600m, account.Balance);
+        
+        }
+        public void Test_Withdraw_InsufficientFunds()
+        {
+            Program account = new Program(500m);
+
+            // Act & Assert (ONLY ONE ASSERT)
+            Assert.AreEqual(
+                "Insufficient funds.",
+                Assert.Throws<Exception>(() => account.Withdraw(800m))!.Message
+            );
+        }
+    }
+}
